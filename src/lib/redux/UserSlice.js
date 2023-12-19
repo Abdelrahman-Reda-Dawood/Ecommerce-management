beed94aaff12c9cd0217eec0af8b8897255f8956
Abdelrahman-Reda-Dawood <116512972+Abdelrahman-Reda-Dawood@users.noreply.github.com>
2023-12-19 16:20:47 +0200

    feat: Included Redux

---
+++ src/lib/redux/UserSlice.js
@@ -0,0 +1,69 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async (userCredentials) => {
+    const request = await axios.post(
+      'https://shopping-api-7cy0.onrender.com/api/auth/login',
+      userCredentials
+    );
+    const response = await request.data;
+    localStorage.setItem('user', JSON.stringify(response));
+    return response;
+  }
+);
+
+export const signupUser = createAsyncThunk(
+  'user/signupUser',
+  async (userCredentials) => {
+    const request = await axios.post(
+      'https://shopping-api-7cy0.onrender.com/api/auth/signup',
+      userCredentials
+    );
+    const response = await request.data;
+    localStorage.setItem('user', JSON.stringify(response));
+    return response;
+  }
+);
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: null,
+    loading: false,
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.user = null;
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.user = null;
+        state.loading = false;
+        console.log(action.error.message);
+        if (
+          action.error.message === 'Request failed with status code 400'
+        ) {
+          state.error = 'Access Denied! Invalid Credentials';
+        } else if (
+          action.error.message === 'Request failed with status code 401'
+        ) {
+          state.error = 'Invalid Credentials';
+        } else {
+          state.error = action.error.message;
+        }
+      });
+  },
+});
+
+export default userSlice.reducer;
