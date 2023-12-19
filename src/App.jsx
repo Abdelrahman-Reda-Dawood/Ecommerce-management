3e8e168bb62e2e949dc237d06bcaf8e622c33afc
Abdelrahman-Reda-Dawood <116512972+Abdelrahman-Reda-Dawood@users.noreply.github.com>
2023-12-19 16:23:40 +0200

    feat: Redirect to register if not logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,139 @@
 import './App.css';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { ThemeContextProvider } from './context/ThemeContext';
+import NotFound from './components/NotFound';
 import RegisterLayout from './components/register/RegisterLayout';
 import Layout from './components/shared/Layout';
+import { ThemeContextProvider } from './context/ThemeContext';
 import AddBrand from './pages/AddBrand';
 import AddCategory from './pages/AddCategory';
+import AddFactory from './pages/AddFactory';
 import AddProducts from './pages/AddProducts';
 import AddShop from './pages/AddShop';
 import AddSupplier from './pages/AddSupplier';
 import AddUser from './pages/AddUser';
 import Dashboard from './pages/Dashboard';
 import ManageOrders from './pages/ManageOrders';
-import Products from './pages/Products';
 import OrderDetails from './pages/OrderDetails';
-import Subcategory from './pages/Subcategory';
 import POS from './pages/POS';
-import NotFound from './components/NotFound';
+import Products from './pages/Products';
+import Subcategory from './pages/Subcategory';
+
+function getUser() {
+  let user = localStorage.getItem('user');
+  if (user) {
+    user = JSON.parse(user);
+  } else {
+    user = null;
+  }
+  return user;
+}
 
 function App() {
+  const [user] = useState(getUser());
+
+  const mainRoute = (
+    <Routes>
+      <Route
+        path="/"
+        element={<Layout />}
+      >
+        <Route
+          index
+          path="/"
+          element={<Dashboard />}
+        />
+        <Route
+          path={`/products`}
+          element={<Products />}
+        />
+        <Route
+          path={`/orders`}
+          element={<ManageOrders />}
+        />
+        <Route
+          path={`/orders/order-details`}
+          element={<OrderDetails />}
+        />
+        <Route
+          path={`/add/product`}
+          element={<AddProducts />}
+        />
+        <Route
+          path={`/add/category`}
+          element={<AddCategory />}
+        />
+        <Route
+          path={`/add/subcategory`}
+          element={<Subcategory />}
+        />
+        <Route
+          path={`/add/brand`}
+          element={<AddBrand />}
+        />
+        <Route
+          path={`/add/shop`}
+          element={<AddShop />}
+        />
+        <Route
+          path={`/add/user`}
+          element={<AddUser />}
+        />
+        <Route
+          path={`/add/supplier`}
+          element={<AddSupplier />}
+        />
+        <Route
+          path={`add/factory`}
+          element={<AddFactory />}
+        />
+      </Route>
+
+      {/* Register */}
+      <Route
+        path={`/register`}
+        element={<RegisterLayout />}
+      />
+
+      <Route
+        path={`/pos`}
+        element={<POS />}
+      />
+      {/* Undefined Routes */}
+      <Route
+        path={`*`}
+        element={<NotFound />}
+      />
+    </Routes>
+  );
+
+  useEffect(() => {
+    console.log({ user });
+    getUser();
+  });
+
   return (
     <ThemeContextProvider>
       <BrowserRouter>
+        {user ? (
+          mainRoute
+        ) : (
+          <Routes>
+            <Route
+              path={`/`}
+              element={<RegisterLayout />}
+            />{' '}
+            <Route
+              path={`*`}
+              element={<NotFound />}
+            />
+          </Routes>
+        )}
+
         <Toaster />
-        <Routes>
-          <Route
-            path="/"
-            element={<Layout />}
-          >
-            <Route
-              index
-              element={<Dashboard />}
-            />
-            <Route
-              path="/manage-product"
-              element={<Products />}
-            />
-            <Route
-              path="/manage-orders"
-              element={<ManageOrders />}
-            />
-            <Route
-              path="/manage-orders/order-details"
-              element={<OrderDetails />}
-            />
-            <Route
-              path="/add-product"
-              element={<AddProducts />}
-            />
-            <Route
-              path="/add-category"
-              element={<AddCategory />}
-            />
-            <Route
-              path="/sub-category"
-              element={<Subcategory />}
-            />
-            <Route
-              path="/add-brand"
-              element={<AddBrand />}
-            />
-            {/* <Route
-              path="/add-coupon"
-              element={<AddCoupon />}
-            /> */}
-            <Route
-              path="/add-shop"
-              element={<AddShop />}
-            />
-            <Route
-              path="/add-user"
-              element={<AddUser />}
-            />
-            <Route
-              path="/add-supplier"
-              element={<AddSupplier />}
-            />
-          </Route>
-          <Route
-            path="/register"
-            element={<RegisterLayout />}
-          />
-          <Route
-            path="/pos"
-            element={<POS />}
-          />
-          <Route
-            path="*"
-            element={<NotFound />}
-          />
-        </Routes>
       </BrowserRouter>
     </ThemeContextProvider>
   );
